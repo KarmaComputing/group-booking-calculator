bb4e7aa58fb9f2b615f2b1abcb792e049c9fa8fc
chrisjsimpson <chris.j.simpson@live.co.uk>
2025-02-25 23:07:45 +0000

    template forms into macros

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, flash, request, redirect, url_for, jsonify
+from flask import Flask, render_template, flash, request, redirect, url_for
 import secrets
 import json
 import pickle
@@ -8,12 +8,13 @@ import pickle
 # Tours
 - Each member *might* have a set cost
 - You can't charge the same for 1 person as a group of 10
-- Set cost example (e.g every partitipent gets entry to an
+- Set cost example (e.g every participant gets entry to an
   attraction)
 """
 
 tour = {
     "name": "London",
+    "tour_code": "LDNPUB2025",
     "pricing_rules": {
         "max_people": 10,
         "min_people": 1,
@@ -69,6 +70,19 @@ def calculate_cost_per_person(tour, number_of_people):
     print(f"Total cost is: {total_cost}")
 
 
+def get_tour_by_tour_code(tour_code, get_index=False):
+    """
+    Return tour object from tours by tour_code or
+    it's tour index if get_index is True
+    """
+    for index, tour in enumerate(tours):
+        if tour["tour_code"] == tour_code:
+            if get_index:
+                return index
+            else:
+                return tour
+
+
 app = Flask(__name__)
 app.secret_key = secrets.token_hex()
 
@@ -78,7 +92,8 @@ try:
     tours = pickle.load(fp)
     tours = json.loads(tours)
 except (FileNotFoundError, EOFError):
-    print("picke file is FileNotFoundError or EOFError. Writing empty tours list")
+    print("pickle file is FileNotFoundError or "
+          "EOFError. Writing empty tours list")
     with open("tours-pickle", mode="wb") as fp:
         stub = json.dumps([])
         pickle.dump(stub, fp)
@@ -106,6 +121,102 @@ def list_tours():
     return render_template("list-tours.html", tours=tours)
 
 
+@app.route("/tour/edit/<tour_code>", methods=["GET", "POST"])
+def edit_tour(tour_code):
+    tour = get_tour_by_tour_code(tour_code)
+    original_tour_code = request.form.get("original_tour_code")
+
+    if request.method == "GET":
+        return render_template("edit_tour.html", tour=tour)
+    elif request.method == "POST":
+        print("Processing tour edit")
+        # Get tour name
+        tour_name = request.form.get("tour_name")
+        tour_code = request.form.get("tour_code")
+        min_people = request.form.get("min_people")
+        max_people = request.form.get("max_people")
+
+        print("Getting price_per_person_group_size_form_controls_count")
+        price_per_person_group_size_form_controls_count = int(
+            request.form["price_per_person_group_size_form_controls_count"]
+        )
+        price_per_person_based_on_size_of_group = []
+
+        for index in range(
+            0, price_per_person_group_size_form_controls_count
+        ):  # noqa: E501
+            min = int(request.form[f"min_people_group_size_price-{index}"])
+            max = int(request.form[f"max_people_group_size_price-{index}"])
+            price_per_person = int(
+                request.form[f"group_size_price_per_person-{index}"]
+            )  # noqa: E501
+
+            price_per_person_based_on_size_of_group.append(
+                {"min": min, "max": max, "price_per_person": price_per_person}
+            )
+
+            if price_per_person_group_size_form_controls_count == 1:
+                print(
+                    "Breaking out of loop price_per_person_group_size_"
+                    "form_controls_count is {price_per_person_group_size_form_controls_count}"  # noqa: E501
+                )
+                break
+
+        print(
+            f"Built object price_per_person_based_on_size_of_group as: {price_per_person_based_on_size_of_group}"  # noqa: E501
+        )
+
+        print("Getting fixed_person_cost_form_controls_count")
+        fixed_person_cost_form_controls_count = request.form[
+            "fixed_person_cost_form_controls_count"
+        ]  # noqa: E501
+
+        fix_costs_per_person = []
+        for fixed_per_person_cost_index in range(
+            0, int(fixed_person_cost_form_controls_count)
+        ):
+            fixed_per_person_cost_name = request.form[
+                f"fixed_per_person_cost_name-{fixed_per_person_cost_index}"
+            ]
+            fixed_per_person_cost_amount = request.form[
+                f"fixed_per_person_cost_amount-{fixed_per_person_cost_index}"
+            ]
+            fix_costs_per_person.append(
+                {
+                    "name": fixed_per_person_cost_name,
+                    "amount": fixed_per_person_cost_amount,
+                }
+            )
+
+            if fixed_person_cost_form_controls_count == 1:
+                print(
+                    "Breaking out of loop fixed_person_cost_form_controls_count"  # noqa: E501
+                    "form_controls_count is {fixed_person_cost_form_controls_count}"  # noqa: E501
+                )
+                break
+
+        # Build and update tour object
+        updated_tour = {
+            "name": tour_name,
+            "tour_code": tour_code,
+            "pricing_rules": {
+                "max_people": max_people,
+                "min_people": min_people,
+                "fix_costs_per_person": fix_costs_per_person,
+                "price_per_person_based_on_size_of_group": price_per_person_based_on_size_of_group,  # noqa: E501
+            },
+            "cost_per_person": "calculate_cost_per_person",
+        }
+
+        original_tour_index = get_tour_by_tour_code(
+            original_tour_code, get_index=True
+        )  # noqa: E501
+        tours[original_tour_index] = updated_tour
+        save_tours_to_pickle_file(tours)
+        flash("Tour edited")
+    return redirect(url_for("list_tours"))
+
+
 @app.route("/api/tour", methods=["POST"])
 def add_tour():
     # Get tour name
@@ -115,56 +226,50 @@ def add_tour():
     max_people = request.form.get("max_people")
 
     # Get fixed costs per person
-    fixed_per_person_cost_names = request.form.getlist(
-        "fixed_per_person_cost_name"
-    )  # noqa: E501
-    fixed_per_person_cost_amounts = request.form.getlist(
-        "fixed_per_person_cost_amount"
+    fix_costs_per_person = []
+    fixed_person_cost_form_controls_count = int(
+        request.form["fixed_person_cost_form_controls_count"]
     )  # noqa: E501
 
-    fix_costs_per_person = dict(
-        zip(fixed_per_person_cost_names, fixed_per_person_cost_amounts)
-    )
+    for index in range(0, fixed_person_cost_form_controls_count):
+        fix_costs_per_person.append(
+            {
+                "name": request.form.get(
+                    f"fixed_per_person_cost_name-{index}"
+                ),  # noqa: E501
+                "amount": request.form.get(
+                    f"fixed_per_person_cost_amount-{index}"
+                ),  # noqa: E501
+            }
+        )
+
     print(f"fix_costs_per_person are {fix_costs_per_person}")
 
-    fix_costs_per_person = [
-        {"name": fixed_pp_cost_name, "amount": fixed_pp_cost_amount}
-        for fixed_pp_cost_name, fixed_pp_cost_amount in zip(
-            fixed_per_person_cost_names, fixed_per_person_cost_amounts
-        )
-    ]
-
     # Get prices per person group size ranges
-    min_people_group_size_prices = request.form.getlist(
-        "min_people_group_size_price"
-    )  # noqa: E501
-
-    max_people_group_size_prices = request.form.getlist(
-        "max_people_group_size_price"
-    )  # noqa: E501
-
-    group_size_price_per_persons = request.form.getlist(
-        "group_size_price_per_person"
-    )  # noqa: E501
-
-    # Combind min/max/price_per_person into structure desired for calculation
-    price_per_person_based_on_size_of_group = [
-        {"min": int(min_p), "max": int(max_p), "price_per_person": int(price)}
-        for min_p, max_p, price in zip(
-            min_people_group_size_prices,
-            max_people_group_size_prices,
-            group_size_price_per_persons,
+    price_per_person_group_size_form_controls_count = int(
+        request.form["price_per_person_group_size_form_controls_count"]
+    )
+    price_per_person_based_on_size_of_group = []
+    for index in range(0, price_per_person_group_size_form_controls_count):
+        price_per_person_based_on_size_of_group.append(
+            {
+                "min": request.form[f"min_people_group_size_price-{index}"],
+                "max": request.form[f"max_people_group_size_price-{index}"],
+                "price_per_person": request.form[
+                    f"group_size_price_per_person-{index}"
+                ],
+            }
         )
-    ]
 
     # Build valid tour object
     tour = {
         "name": tour_name,
+        "tour_code": tour_code,
         "pricing_rules": {
             "max_people": int(max_people),
             "min_people": int(min_people),
             "fix_costs_per_person": fix_costs_per_person,
-            "price_per_person_based_on_size_of_group": price_per_person_based_on_size_of_group,
+            "price_per_person_based_on_size_of_group": price_per_person_based_on_size_of_group,  # noqa: E501
         },
     }
 
